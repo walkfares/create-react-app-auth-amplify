3ba546d9fd70abc3e6ffa0d8d697efeffda67d3d
Colin Hartley <colin.hartley@walkfares.com>
2019-10-04 12:21:16 +0100

    test

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Walkfares</h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
